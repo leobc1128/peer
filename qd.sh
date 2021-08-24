dfbfd8e18acaad8fd566316727ce07386b6bde36
leobc1128 <89430860+leobc1128@users.noreply.github.com>
2021-08-24 14:59:22 +0800

    Add files via upload

---
+++ qd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+	 num=$1
+ else 
+	  num=30
+  fi
+  for ((i=1;i<=$num;i++))
+  do
+	   docker run -d --restart=on-failure notfourflow/p2pclient liubc1128@gmail.com
+   done
