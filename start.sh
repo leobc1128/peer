bc7f627f94ce163c009ac86e32e6d3e480ea8a22
leobc1128 <89430860+leobc1128@users.noreply.github.com>
2021-08-25 14:15:51 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+ num=$1
+else 
+ num=60
+fi
+for ((i=1;i<=$num;i++))
+do
+ docker run -d --restart=on-failure notfourflow/p2pclient:alpine liubc1128@gmail.com
+done
