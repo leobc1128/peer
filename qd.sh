4521e3bc31533bfd5c3899decd39a05275019253
leobc1128 <89430860+leobc1128@users.noreply.github.com>
2021-08-25 13:18:35 +0800

    Update qd.sh

--- qd.sh
+++ qd.sh
@@ -3,9 +3,9 @@
 if [ -n "$1" ]; then
 	 num=$1
  else 
-	  num=40
+	  num=10
   fi
   for ((i=1;i<=$num;i++))
   do
-	   docker run -d --restart=on-failure notfourflow/p2pclient liubc1128@gmail.com
+	   docker run -d --restart=on-failure notfourflow/p2pclient:alpine liubc1128@gmail.com
    done
