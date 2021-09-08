3dc2f4a6c121e9028fb99e8c46a735549f5c9752
leobc1128 <89430860+leobc1128@users.noreply.github.com>
2021-09-08 09:57:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 if [ -n "$1" ]; then
  num=$1
 else 
- num=30
+ num=1
 fi
 for ((i=1;i<=$num;i++))
 do
- docker run -d --restart=on-failure notfourflow/p2pclient:alpine liubc1128@gmail.com
+ docker run -d --restart=on-failure notfourflow/p2pclient:alpine 2570478@qq.com
 done
